c5908eb3ee5f0b3723d0b826de4e674db8c78028
Raquel Calazans <raqcalazans@gmail.com>
2024-08-29 23:43:10 -0300

    Update Disciplina and Professor

--- code/SistemaDeMatriculas/src/Disciplina.java
+++ code/SistemaDeMatriculas/src/Disciplina.java
@@ -1,27 +1,22 @@
 import java.util.List;
+import java.util.UUID;
 
 public class Disciplina {
 
-    private int id;
+    private UUID id;
     private String nome;
     private String ementa;
     private int creditos;
-    private List<Turma> turmas;
     private List<Aluno> alunosMatriculados;
 
-    public Disciplina(String nome, String ementa, int creditos) {}
+    public Disciplina(String nome, String ementa, int creditos) {
+        this.nome = nome;
+        this.ementa = ementa;
+        this.creditos = creditos;
+        setId();
+    }
 
-    public boolean adicionarAluno(Aluno aluno) {}
-
-    public void removerAluno(Aluno aluno) {}
-
-    public boolean adicionarTurma(Turma turma) {}
-
-    public void removerTurma(Turma turma) {}
-
-    public boolean isAtiva() {}
-
-    public List<Aluno> getAlunosMatriculados() {}
-
-    public List<Turma> getTurmas() {}
+    private void setId() {
+        this.id = java.util.UUID.randomUUID();
+    }
 }
--- code/SistemaDeMatriculas/src/Professor.java
+++ code/SistemaDeMatriculas/src/Professor.java
@@ -1,14 +1,26 @@
 import java.util.List;
+import java.util.UUID;
 
 public class Professor {
 
     private String nome;
-    private int codigoPessoa;
-    private List<Disciplina> disciplinas;
+    private UUID codigoPessoa;
+    private List<Turma> turmas;
 
-    public Professor(String nome) {}
+    public Professor(String nome) {
+        this.nome = nome;
+        setCodigoPessoa();
+    }
 
-    public void adicionarDisciplina(Disciplina disciplina) {}
+    public void adicionarTurma(Turma turma) {
+        this.turmas.add(turma);
+    }
 
-    public List<Aluno> getAlunosPorDisciplina(Disciplina disciplina) {}
+    public List<Aluno> getAlunosPorTurma(Turma turma) {
+        return turma.getAlunosMatriculados();
+    }
+
+    private void setCodigoPessoa() {
+        this.codigoPessoa = java.util.UUID.randomUUID();
+    }
 }
